4d7990f80cf4f1d329f4b6c096502138a7199ab2
Kai Stullich <kairstullich@gmail.com>
2018-01-29 21:52:16 -0800

    removed keys for PR

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mongoURI: 'mongodb://kai:pusherpoll@ds023054.mlab.com:23054/pusher_poll_dev',
-  pusherAppId: '465579',
-  pusherKey: '55a3916fa22efde02378',
-  pusherSecret: '7d5777e4107f1fb9c64f',
-  pusherCluster: 'us2',
+  mongoURI: '',
+  pusherAppId: '',
+  pusherKey: '',
+  pusherSecret: '',
+  pusherCluster: '',
   pusherEncrypted: true
 };
